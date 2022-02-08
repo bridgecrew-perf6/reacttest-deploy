24c1c1b8a9b16d9348ab27ff2ebcd5a528d8b77a
sachin <saccu180693@gmail.com>
2022-02-09 01:24:45 +0530

    Deploy react app

---
+++ src/components/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        image:'https://th.bing.com/th/id/OIP.xunZmL1dSKF6nssAGi9p1wHaE8?w=229&h=180&c=7&r=0&o=5&pid=1.7'
+    },
+    {
+        image:'https://th.bing.com/th/id/OIP.HenRJHHKfbIDNM2yzwdkkgHaEV?w=291&h=180&c=7&r=0&o=5&pid=1.7'
+    },
+    {
+        image:'https://th.bing.com/th/id/OIP.q2F1sEzDy1YOeupxKJ1DdAHaEo?pid=ImgDet&rs=1'
+    }
+]
